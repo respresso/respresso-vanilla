6be13b583fee62e111040861f36c5aa7f5e5282a
Velez Krisztián <velkrisztian@gmail.com>
2019-09-03 17:47:06 +0200

    Add some content to the sample page which uses the following resources from Respresso: Localization, Color (scss), Images, Inline images (scss) Change demo Respresso project token to the new one.

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,13 @@
 import {messages} from "./messages";
 
 document.querySelectorAll("[data-translate]").forEach((element: Element) => {
-	if (element instanceof HTMLElement) {
-		const ex = element.dataset['translate'];
-		const [k, v] = ex.split("=");
-		element[k] = messages.en.profile[v];
-	}
+    if (element instanceof HTMLElement) {
+        const ex = element.dataset['translate'];
+        if (ex.indexOf("=") >= 0) {
+            const [k, v] = ex.split("=");
+            element.setAttribute(k, messages.en[v]);
+        } else {
+			element.innerText = messages.en[ex];
+        }
+    }
 });
--- src/messages.ts
+++ src/messages.ts
@@ -1,4 +1,4 @@
-const english = require("../respresso/localization/localization-en.json");
+const english = require("../respresso/localization/localization-en-flat.json");
 
 export const messages = {
 	en: english
